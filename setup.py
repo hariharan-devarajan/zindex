5af3f16d4fe0d1882be6b5a034a17dcc66765985
Ray Andrew <raydreww@gmail.com>
2025-08-08 17:27:13 -0700

    add setup_requires

--- setup.py
+++ setup.py
@@ -146,6 +146,7 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
     ],
+    setup_requires=["pybind11==2.13.6", "setuptools>=64", "wheel", "cmake>=3.12"],
     install_requires=["pybind11==2.13.6", "setuptools>=64", "wheel", "cmake>=3.12"],
     keywords="profiler, deep learning, I/O, benchmark, NPZ, pytorch benchmark, tensorflow benchmark",
     project_urls={  # Optional
